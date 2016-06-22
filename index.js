7ae1832bbb73441711a91e8cb8218e7c7a95f83c
Maurice Prosper <maurice.prosper@ttu.edu>
2016-06-22 18:53:26 -0500

    Added base kmeans and clustering

---
+++ index.js
@@ -0,0 +1,113 @@
+module.exports = KMeans;
+
+// dependencies
+const Color = require('./lib/color');
+const Cluster = require('./lib/cluster');
+
+/**
+ * Constructor
+ * @param colors int
+ * @param groups int
+ */
+function KMeans(colors, groups) {
+    if(groups > colors && groups <= 1)
+        throw Error('More groups than colors given');
+
+    this.groups = new Array(groups);
+    this.colors = new Array(colors);
+
+    
+    for(var i = 0; i < this.colors.length; i++)
+        this.colors[i] = Color({
+            hue: parseInt(Math.random() * 360),
+            saturation: Math.random(),
+            lightness: Math.random(),
+        });
+
+    // choose random start points for groups
+    for(var i = 0; i < this.groups.length; i++) {
+        this.groups[i] = new Cluster('Cluster ' + i);
+        this.groups[i].add(this.colors[i]).centroid(); // save center
+    }
+}
+
+/**
+ * Places a color in the closest group
+ */
+KMeans.prototype.findGroups = function() {
+    var centroids = [];
+
+    // store last clusters & clear them
+    this.groups.forEach(function(cluster) {
+        cluster.colors.forEach(function (color) {
+            color.lastGroup = cluster.name;
+        });
+
+        centroids.push( cluster.centroid() );
+        
+        cluster.clear();
+    });
+    
+    // find distance from each point to all groups
+    this.colors.forEach(function (color) {
+        var minDist, minIndex = 0, tmp;
+
+        // find closest
+        centroids.forEach(function (centroid, i) {
+            tmp = color.distance( centroid );
+
+            if(typeof minDist === 'undefined' || tmp < minDist) {
+                minIndex = i;
+                minDist = tmp;
+            }
+        });
+
+        // update color info
+        this.groups[ minIndex ].add( color );
+    }.bind(this));
+};
+
+/**
+ * Check any color changed their previous cluster
+ * @returns {boolean}
+ */
+KMeans.prototype.changed = function() {
+    var ret = false;
+
+    this.groups.some(function (cluster) {
+        return cluster.colors.some(function (color) {
+            if(color.lastGroup !== cluster.name ||typeof color.lastGroup === 'undefined') {
+                ret = true;
+                return true;
+            }
+        });
+    });
+    
+    return ret;
+};
+
+/**
+ * Keeps clustering until groups no longer change
+ * @param verbosity
+ */
+KMeans.prototype.solve = function (verbosity) {
+    verbosity = parseInt(verbosity) || 0;
+    
+    var i = 1;
+    
+    do {
+        if(verbosity >= 1)
+            console.log('Iteration %d', i++);
+        
+        this.findGroups();
+
+        if(verbosity >= 2)
+            this.groups.forEach(function(cluster) {
+                console.log('%s has %d colors, its centriod is ',
+                    cluster.name,
+                    cluster.colors.length,
+                    cluster.centroid()
+                );
+            });
+    } while(this.changed());
+};
